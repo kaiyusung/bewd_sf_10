efefb0d48a52699c1e6759275a36d88a6fc7ba95
kaiyusung <edward.kaiyu@gmail.com>
2015-09-13 23:31:40 -0700

    lesson_11

--- 11_Rails_Intro_Controller/bookazon_app/app/controllers/books_controller.rb
+++ 11_Rails_Intro_Controller/bookazon_app/app/controllers/books_controller.rb
@@ -1,8 +1,16 @@
 class BooksController < ApplicationController
+
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
   end
 
   #READ: this will retrieve all books through active_record
@@ -11,12 +19,20 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = get_book
   end
 
   def edit
+    @book = get_book
   end
 
   def update
+    @book = get_book
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -25,6 +41,11 @@ class BooksController < ApplicationController
   private
 
   def book_params
+    params.require(:book).permit(:title, :year_published, :author, :available, :genre, :image_url)
+  end
+
+  def get_book
+    Book.find(params[:id])
   end
 
 
